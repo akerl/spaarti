b8541a3be0980738547a2efbf56694a38df653e0
Les Aker <me@lesaker.org>
2016-01-23 16:57:40 -0500

    bump version, fixes #8

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 1.0.0 / 2016-01-23
+
+* [ENHANCEMENT] Stabilized API
+
--- lib/spaarti/version.rb
+++ lib/spaarti/version.rb
@@ -2,5 +2,5 @@
 ##
 # Set the version (needed for Mercenary -v)
 module Spaarti
-  VERSION = '0.1.1'.freeze
+  VERSION = '1.0.0'.freeze
 end
